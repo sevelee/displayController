f5db494f6593eec98ac41802df7f6db15b3d9a9d
sevelee <seve.lee@foxmail.com>
2017-09-22 23:30:04 +0800

    新建了类，DisplayContorller 来控制硬件的更新。 写了一半。 明天继续。

---
+++ Assets/DisplayController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayController{
+    string SerialPort;
+    int length, width;
+    int[,] LastPicture;
+    int[,] Picture;
+
+    public int Length
+    {
+        get { return length; }
+    }
+
+    public int Width
+    {
+        get { return width; }
+    }
+
+    /// <summary>
+    /// 设置显示大小
+    /// </summary>
+    /// <param length="x"></param>
+    /// <param width="y"></param>
+    public void SetSize(int x, int y)
+    {
+        length = x;
+        width = y;
+    }
+
+    void showPicture()
+    {
+        return;
+    }
+}
