477851339afd0154d20a009513e7645a81f6bbe7
sevelee <seve.lee@foxmail.com>
2017-09-23 15:21:23 +0800

    乱写一气

--- Assets/DisplayController.cs
+++ Assets/DisplayController.cs
@@ -1,21 +1,55 @@
-﻿using System.Collections;
+﻿//
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO.Ports;
+using System.Threading;
 
 public class DisplayController{
-    string SerialPort;
+    string serialPortName;
     int length, width;
-    int[,] LastPicture;
-    int[,] Picture;
+    int hitterNumber = 6;
+    int[,] lastPicture;
+    int[,] picture;
+    SerialPort myPort;
+
+    //
+    Thread receiveThread;
+    Thread sendThread;
 
     public int Length
-    {
+    { 
         get { return length; }
     }
 
     public int Width
     {
         get { return width; }
+    }
+
+    public int[,] LastPicture
+    {
+        get { return lastPicture; }
+    }
+
+    public int[,] Picture
+    {
+        get { return picture; }
+        set { picture = value; }
+    }
+
+    public void Init(string SerialPortName, int speed)
+    {
+        serialPortName = SerialPortName;
+        myPort = new SerialPort(serialPortName, speed, Parity.None, 8, StopBits.One);
+        myPort.ReadTimeout = 1000;
+        myPort.Open();
+
+        if (myPort.IsOpen)
+        {
+            Debug.Log("port opened.");
+        }
     }
 
     /// <summary>
@@ -29,8 +63,12 @@ public class DisplayController{
         width = y;
     }
 
-    void showPicture()
+    /// <summary>
+    /// 将hitter的数据压缩一下
+    /// </summary>
+    void convertData (int[,] data)
     {
+
         return;
     }
 }
---
+++ Assets/tester.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class tester : MonoBehaviour {
+    DisplayController myController;
+
+	// Use this for initialization
+	void Start () {
+        myController = new DisplayController();
+        string[] portList = SerialPort.GetPortNames();
+        for (int i = 0; i < portList.Length; i++)
+        {
+            Debug.Log(portList[i]);
+        }
+
+        myController.Init("COM8", 115200);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnGUI()
+    {
+    }
+}
