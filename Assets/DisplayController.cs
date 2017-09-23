d9142e0d7f2e5697334447a5b671cf462d773823
sevelee <seve.lee@foxmail.com>
2017-09-23 20:21:57 +0800

    增加了上下左右调试的按钮。待测试。

--- Assets/DisplayController.cs
+++ Assets/DisplayController.cs
@@ -12,7 +12,8 @@ public class DisplayController{
     int hitterNumber = 6;
     int[,] lastPicture;
     int[,] picture;
-    SerialPort myPort;
+    public SerialPort myPort;
+    bool getResponse;
 
     //
     Thread receiveThread;
@@ -45,10 +46,75 @@ public class DisplayController{
         myPort = new SerialPort(serialPortName, speed, Parity.None, 8, StopBits.One);
         myPort.ReadTimeout = 1000;
         myPort.Open();
+        getResponse = true;
 
         if (myPort.IsOpen)
         {
             Debug.Log("port opened.");
+        }
+    }
+
+    /// <summary>
+    /// 发送移动位（相对），回头改成pravate
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    public void sendMove(int x, int y)
+    {
+        Thread rThread;
+        // 如果上一次通讯得到了回复（即，arduino完成了动作），再发送这次动作。
+        if (getResponse)
+        {
+            myPort.Write("Move," + x + "," + y);
+            getResponse = false;
+            rThread = new Thread(check_r_thread);
+            rThread.IsBackground = true;
+            rThread.Start();
+        }
+    }
+
+    /// <summary>
+    /// 发送一组sData
+    /// </summary>
+    /// <param name="sData"></param>
+    public void sendData(int[] sData)
+    {
+        Thread rThread;
+        if (getResponse)
+        {
+            string sendString = "Hit,";
+            for (int i = 0; i < sData.Length; i++)
+            {
+                sendString += sData[i] + ",";
+            }
+            myPort.Write(sendString);
+            getResponse = false;
+            rThread = new Thread(check_r_thread);
+            rThread.IsBackground = true;
+            rThread.Start();
+        }
+    }
+
+    /// <summary>
+    /// 查看arduino是否进行了回复，Move和Hit都可以用。
+    /// </summary>
+    private void check_r_thread()
+    {
+        while (true)
+        {
+            try
+            {
+                string strRec = myPort.ReadLine();
+                Debug.Log(strRec);
+                if (strRec == "o")
+                {
+                    getResponse = true;
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+            }
         }
     }
 
--- Assets/tester.cs
+++ Assets/tester.cs
@@ -25,5 +25,33 @@ public class tester : MonoBehaviour {
 
     private void OnGUI()
     {
+        if (GUI.Button(new Rect(10, 10, 200, 50), "move test"))
+        {
+            myController.sendMove(2, 2);
+        }
+        if (GUI.Button(new Rect(210, 10, 200, 50), "hit test"))
+        {
+            int[] temp;
+            temp = new int[] { 1, 1, 1, 1, 1, 1 };
+            myController.sendData(temp);
+        }
+
+        // 上下左右
+        if (GUI.Button(new Rect(10, 200, 50, 50), "left"))
+        {
+            myController.sendMove(1, 0);
+        }
+        if (GUI.Button(new Rect(110, 200, 50, 50), "right"))
+        {
+            myController.sendMove(-1, 0);
+        }
+        if (GUI.Button(new Rect(60, 150, 50, 50), "up"))
+        {
+            myController.sendMove(0, -1);
+        }
+        if (GUI.Button(new Rect(60, 200, 50, 50), "down"))
+        {
+            myController.sendMove(0, 1);
+        }
     }
 }
